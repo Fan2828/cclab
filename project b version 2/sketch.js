76364bdb11e3dc2f59c04d8ee86a3dce611436ce
Fan2828 <cy2180@nyu.edu>
2022-05-01 19:42:02 +0800

    some changes

--- project b version 2/sketch.js
+++ project b version 2/sketch.js
@@ -1,28 +1,31 @@
 let bikeX=10,bikeY=30;
 function setup() {
+  
   let canvas=createCanvas(windowWidth, 70);
   canvas.parent('container');
 }
 
 function draw() {
-  // background(200,50);
-  clear();
+  background(200,100);
+  // clear();
   fill(100,0,0,100);
   circle(bikeX,bikeY,50);
   if(mouseX>bikeX){
     if(mouseIsPressed){
       bikeX=bikeX+1;
   }
- 
-  }else{
+
+  }
+  else{
     if(mouseIsPressed){
       bikeX=bikeX-1;
   }
   }
-  if (keyIsPressed){
-    let elems = document.querySelectorAll('#map');
-    elems[0].style.visibility = 'visible';
-  }
-
+  // if (mouseIsPressed){
+  //   let elems = document.querySelector('.toppings');
+  //   elems[0].style.visibility = 'visible';
+  // }
+  let elems = document.querySelector('.toppings');
+  elems[0].style.visibility = 'visible';
 
 }
