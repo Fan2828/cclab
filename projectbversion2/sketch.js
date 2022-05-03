960201a02ca905cf1f08496b4232e3dd9103da29
Fan2828 <cy2180@nyu.edu>
2022-05-03 10:00:35 +0800

    project b prototype

--- projectbversion2/sketch.js
+++ projectbversion2/sketch.js
@@ -1,8 +1,21 @@
 let bikeX=10,bikeY=30;
+let toppings=[];
+let elements=document.querySelectorAll('.toppings');
+
 function setup() {
   
   let canvas=createCanvas(windowWidth, 70);
   canvas.parent('container');
+
+  // let elements=document.querySelectorAll('.toppings');
+  for (let i=0;i<elements.length;i++){
+    let topping=new Topping (elements[i].offsetLeft,elements[i].offsetWidth);
+    toppings.push(topping);
+    console.log('topping at', elements[i].offsetLeft, elements[i].offsetTop, 'dimensions', elements[i].offsetWidth, elements[i].offsetHeight);
+    
+  }
+
+
 }
 
 function draw() {
@@ -21,11 +34,32 @@ function draw() {
       bikeX=bikeX-1;
   }
   }
-  // if (mouseIsPressed){
-  //   let elems = document.querySelector('.toppings');
-  //   elems[0].style.visibility = 'visible';
-  // }
-  let elems = document.querySelector('.toppings');
-  elems[0].style.visibility = 'visible';
+for(let i=0;i<toppings.length;i++){
+  if(elements[i].offsetLeft<bikeX){
+    elements[i].style.visibility = 'visible';
+  }
+}
+
+  
+
+  
 
 }
+
+class Topping{
+  constructor(x,w){
+    this.x=x;
+    this.w=w;
+  }
+   isNearby(checkX,checkY){
+     if (this.x<checkX&&checkX<this.x+this.w){
+       return true;
+     }else{
+       return false;
+     }
+   }
+}
+
+// function windowResized(){
+//   resizeCanvas(windowWidth,70);
+// }
