3a3d2d8376f8129287247a1d4629795b2eb402ff
Fan2828 <cy2180@nyu.edu>
2022-05-07 14:51:07 +0800

    filter

--- projectbversion2/sketch.js
+++ projectbversion2/sketch.js
@@ -1,10 +1,15 @@
-let bikeX=30,bikeY=35;
+let bikeX=80,bikeY=90;
 let toppings=[];
 let elements=document.querySelectorAll('.toppings');
 let bikes=[];
 let curImage=0;
 let bikeWidth=100;
 let bikeHeight=100;
+let bikeSpeed=0;
+let bikeMoved;
+let canvasWidth=3000;
+let scences;
+
 
 
 
@@ -17,7 +22,7 @@ bikes.push(loadImage('asset/bike2.png'));
 
 function setup() {
   
-  let canvas=createCanvas(3000, 200);
+  let canvas=createCanvas(canvasWidth, 200);
   canvas.parent('container');
 
   let elements=document.querySelectorAll('.toppings');
@@ -27,6 +32,10 @@ function setup() {
     console.log('topping at', elements[i].offsetLeft, elements[i].offsetTop, 'dimensions', elements[i].offsetWidth, elements[i].offsetHeight);
     
   }
+  scences=document.querySelectorAll('#sunset');
+  // concole.log(scences);
+  sunsetX=scences[0].offsetLeft+scences[0].offsetWidth/2;
+  console.log('center of the sunset is at'+sunsetX);
 
 
 
@@ -39,54 +48,56 @@ function draw() {
   background(200,100);
   clear();
   fill(100,0,0,100);
+  //draw the ground
   noStroke();
- let ground=quad(50,bikeY+70,windowWidth,bikeY+70,windowWidth-50,bikeY+bikeHeight+10,0,bikeY+bikeHeight+10);
+ let ground=quad(50,bikeY+15,canvasWidth,bikeY+15,canvasWidth-50,bikeY+bikeHeight/2+20,0,bikeY+bikeHeight/2+20);
+
+ //move the bike
+ bikeX=bikeX+bikeSpeed;
+
+ push();
+ translate(bikeX,bikeY);
+ if(mouseX<bikeX){
+   scale(-1,1);
+ }
+ imageMode(CENTER);
+ image(bikes[curImage],0,0,bikeWidth,bikeHeight);
+ pop();
 
 
- image(bikes[curImage],bikeX,bikeY,bikeWidth,bikeHeight);
- 
- 
- 
- console.log(bikeX,mouseX);
- 
 
-  
+ if(mouseIsPressed&&mouseX>bikeX){
+   bikeSpeed=1;
+ }else if(mouseIsPressed&&mouseX<=bikeX){
+   bikeSpeed=-1;
+ }else{
+  bikeSpeed=0;
+ }
+  //  console.log(bikeSpeed);
+ 
+//animation
   if(frameCount%10==0){
     curImage=(curImage+1)%bikes.length;
   }
- 
 
-
-
-  // circle(bikeX,bikeY,50);
-  if(mouseX>bikeX+bikeWidth){
-    if(mouseIsPressed){
-      bikeX=bikeX+1;
-  }
-
-  }
-
-
-  else{
-    if(mouseIsPressed){
-      push();
-      scale(-1,1);
-      bikeX=bikeX-1;
-      pop();
-  }
-  }
-// for(let i=0;i<toppings.length;i++){
-//   if(elements[i].offsetLeft<bikeX&&elements[i].offsetLeft+elements.offsetWidth>bikeX){
-//     elements[i].style.visibility = 'visible';
-//   }
-// }
+  //pop up
 
 for(let i=0;i<toppings.length;i++){
-  if(elements[i].offsetLeft<bikeX){
+  if(elements[i].offsetLeft<bikeX+bikeWidth/2&&elements[i].offsetLeft+elements[i].offsetWidth>bikeX+bikeWidth/2){
     elements[i].style.visibility = 'visible';
+    // console.log(elements[i].offsetLeft+elements[i].offsetWidth);
+    // console.log(bikeX);
   }
 }
-console.log(elements[1].offsetLeft+elements.offsetWidth);
+//filter
+
+let saturation = 1 - (abs(sunsetX - bikeX) / 200);
+saturation = constrain(saturation, 0.1, 1);
+ scences[0].style.filter = 'saturate(' + saturation + ')';
+//  console.log(scences[0].style.filter);
+
+
+// console.log(scences[0].style.filter);
 
 }
 
@@ -104,6 +115,6 @@ class Topping{
   //  }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,200);
-}
+// function windowResized(){
+//   resizeCanvas(windowWidth,200);
+// }
